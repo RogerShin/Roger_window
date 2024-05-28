7d171974334494ed66c8be7cc916cf6da2d792fb
Roger <roger02771@gmail.com>
2024-05-28 11:12:13 +0800

    create index.py

---
+++ window/index.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+with open('names.txt', encoding='utf-8') as files:
+    content:str = files.read()
+
+names:list[str] = content.split()
+for name in names:
+    print(name)
