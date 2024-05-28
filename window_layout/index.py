7a286dd16255a2b6bf3c3dd7a51e4ddf4320cf98
Roger <roger02771@gmail.com>
2024-05-28 15:36:24 +0800

    modify button, geometry

--- window_layout/index.py
+++ window_layout/index.py
@@ -7,15 +7,20 @@ class Window(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.title("pack1")
+        # 設定視窗大小
+        self.geometry('300x200')
+        # btn1:ttk.Button = ttk.Button(self, text = "Top")
+        # btn1.pack()
 
-        btn1:ttk.Button = ttk.Button(self, text = "Top")
-        btn1.pack()
+        # btn2:ttk.Button = ttk.Button(self, text = "Middle")
+        # btn2.pack()
 
-        btn2:ttk.Button = ttk.Button(self, text = "Middle")
-        btn2.pack()
+        # btn3:ttk.Button = ttk.Button(self, text = "Bottom")
+        # btn3.pack()
 
-        btn3:ttk.Button = ttk.Button(self, text = "Bottom")
-        btn3.pack()
+        ttk.Button(self, text = "Top").pack()
+        ttk.Button(self, text = "Middle").pack()
+        ttk.Button(self, text = "Bottom").pack()
 
 if __name__ == '__main__':
     window:Window = Window()
