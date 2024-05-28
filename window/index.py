b0d9cddb08da5e7b155be1650ef0c4e4030aae90
Roger <roger02771@gmail.com>
2024-05-28 14:11:52 +0800

    modify title value

--- window/index.py
+++ window/index.py
@@ -9,13 +9,13 @@ def get_names() -> list[str]:
     return names
 
 class Window(tk.Tk):
-    def __init__(self):
+    def __init__(self, title:str = "Hello! Tkinter!"):
         super().__init__()
         # 多做一些事
-        self.title("我是第一個GUI程式")
+        self.title(title)
 
 if __name__ == '__main__':
     names:list[str] = get_names()
-    window:Window = Window()
+    window:Window = Window(title = "我是第一個GUI程式")
     window.mainloop()
     
