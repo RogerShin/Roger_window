56788b0f24e2f7ee7bcdda5975fe8bf47c365c44
Roger <roger02771@gmail.com>
2024-05-28 13:55:40 +0800

    modify add class

--- window/index.py
+++ window/index.py
@@ -8,9 +8,16 @@ def get_names() -> list[str]:
     names:list[str] = content.split()
     return names
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # 多做一些事
+        self.title("我是第一個GUI程式")
+
+
+
 if __name__ == '__main__':
     names:list[str] = get_names()
-    window:tk.Tk = tk.Tk()
-    window.title("我是第一個GUI程式")
+    window:Window = Window()
     window.mainloop()
     
