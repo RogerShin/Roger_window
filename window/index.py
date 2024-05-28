6ce9758bc87a92c7bca678e33ef78bb27a78b5b6
Roger <roger02771@gmail.com>
2024-05-28 11:24:22 +0800

    modify

--- window/index.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("cls")
-with open('names.txt', encoding='utf-8') as files:
-    content:str = files.read()
-
-names:list[str] = content.split()
-# for name in names:
-#     print(name)
