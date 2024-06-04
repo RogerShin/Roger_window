112f44fcb3f0b8d06f41f898afb17093b09db37f
Roger <roger02771@gmail.com>
2024-06-04 11:13:28 +0800

    index.py

---
+++ 2024_06_04/index.py
@@ -0,0 +1,34 @@
+import requests
+from requests import JSONDecodeError
+from pprint import pprint
+import os
+os.system("cls")
+
+def download_json() -> dict[any]:
+    aqi_url = "	https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate desc&format=JSON"
+
+    try:
+        response = requests.get(aqi_url)
+    except Exception as error:
+        # 錯誤的訊息在初始化
+        raise Exception("連線失敗")
+    else:
+        if response.status_code == 200:
+            try:
+                # 這是python的資料結構
+                all_data:dict[any] = response.json()
+                return all_data
+            except JSONDecodeError:
+                raise Exception("api_key為測試用,連線已至上限,請稍後再試")
+        else:
+            raise Exception("下載狀態碼不是200")
+
+def main():
+    try:
+        all_data:dict[any] = download_json()
+        pprint(all_data['records'])
+    except Exception as error:
+        print(error)
+
+if __name__ == '__main__':
+    main()
