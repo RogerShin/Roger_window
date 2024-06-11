73d090a98f57476bf32784db224cb002ad22b1e4
Roger <roger02771@gmail.com>
2024-06-10 21:23:34 -0600

    作業講解

--- 2024_06_11/index.py
+++ 2024_06_11/index.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from tkinter import messagebox
 
 
 class Window(ThemedTk):
@@ -27,28 +28,34 @@ class Window(ThemedTk):
         label_name = tk.Label(input_frame, text="姓名:")
         label_name.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
 
-        entry_name = tk.Entry(input_frame)
-        entry_name.grid(row=0, column=1, padx=5, pady=5)
+        self.entry_name = tk.Entry(input_frame)
+        self.entry_name.grid(row=0, column=1, padx=5, pady=5)
 
         # 身高體重
         label_height = ttk.Label(input_frame, text="身高 (cm):")
         label_height.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
 
-        entry_height = ttk.Entry(input_frame)
-        entry_height.grid(row=1, column=1, padx=5, pady=5)
+        self.entry_height = ttk.Entry(input_frame)
+        self.entry_height.grid(row=1, column=1, padx=5, pady=5)
 
         label_weight = ttk.Label(input_frame, text="體重 (kg):")
         label_weight.grid(row=2, column=0, padx=5, pady=5, sticky=tk.E)
 
-        entry_weight = ttk.Entry(input_frame)
-        entry_weight.grid(row=2, column=1, padx=5, pady=5)
+        self.entry_weight = ttk.Entry(input_frame)
+        self.entry_weight.grid(row=2, column=1, padx=5, pady=5)
 
         button_calculate =ttk.Button(self, text="計算", command=self.show_bmi_result, style='press.TButton')
-        button_calculate.pack(side=tk.RIGHT, padx=(0,35), pady=(10), ipadx=10, ipady=15)
+        button_calculate.pack(side=tk.RIGHT, padx=(0,35), pady=(10), ipadx=5, ipady=5)
 
 
     def show_bmi_result(self):
-        print("計算")
+        try:
+            name:str = self.entry_name.get()
+            height:int = int(self.entry_height.get())
+            weight:int = int(self.entry_weight.get())
+        except Exception as error:
+            messagebox.showerror("Warning", "格式錯誤或欄位沒有填寫")
+        print(name, height, weight)
 
 def main():
     window = Window(theme='arc')
