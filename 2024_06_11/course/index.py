25330df8019fd137d751c7d745b9579990261843
Roger <roger02771@gmail.com>
2024-06-11 02:36:53 -0600

    整合先前的代碼

--- 2024_06_11/course/index.py
+++ 2024_06_11/course/index.py
@@ -1,8 +1,19 @@
-import data
+import ubikedata
+from ttkthemes import ThemedTk
+
+class Window(ThemedTk):
+    def __init__(self, theme:str|None, **kwargs):
+        super().__init__(**kwargs)
+        try:
+            ubike:list[dict] = ubikedata.load_data()
+        except Exception as error:
+            print(error)
+        else:
+            print(ubike)
 
 def main():
-    ubike:list[dict] = data.load_data()
-    print(ubike)
+    window = Window(theme='arc')
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
