4f94f2b9e971abc4858f1f6403882c19d533065d
Roger <roger02771@gmail.com>
2024-06-14 16:45:55 +0800

    tkintermapview , set_position, set_zoom, marker 座標, 座標上顯示資料, set_path

--- 2024_06_14_01/tools.py
+++ 2024_06_14_01/tools.py
@@ -17,18 +17,22 @@ class CustomMessagebox(Dialog):
         #====================
         map_frame = ttk.Frame(contain_frame)
         map_widget = tkmap.TkinterMapView(map_frame, width=800,height=600,corner_radius=0)
-
         map_widget.pack()
-        marker = map_widget.set_position(self.site.lat, self.site.lng, marker=True) # 抓經緯度位置, marker座標
+
+        # 抓經緯度位置, marker座標
+        marker = map_widget.set_position(self.site.lat, self.site.lng, marker=True) 
         marker.set_text(f"{self.site.sarea}\n{self.site.sna}\n總車輛:{self.site.total}\n可借:{self.site.rent_bikes}\n可還:{self.site.retuen_bikes}")
+        
+        # set_path
         start_point = self.site.lat+0.0005, self.site.lng+0.0005
         end_point = start_point[0], start_point[1]-0.001
         down_point = end_point[0] - 0.001, end_point[1]
         left_point = down_point[0], down_point[1] +0.001
         up_point = left_point[0] + 0.001, left_point[1]
         map_widget.set_path([(start_point), (end_point), (down_point), (left_point), (up_point)])
-        map_widget.set_zoom(20) # 顯示大小
 
+        # 顯示大小
+        map_widget.set_zoom(20) 
         map_frame.pack(expand=True, fill='both')     
         #====================
         contain_frame.pack(expand=True, fill='both', pady=10,padx=30)
