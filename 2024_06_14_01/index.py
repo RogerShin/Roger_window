31948e58574a63a69e314a04cdcd773f7f6804bf
Roger <roger02771@gmail.com>
2024-06-14 09:34:22 +0800

    modify

--- 2024_06_14_01/index.py
+++ 2024_06_14_01/index.py
@@ -1,14 +1,22 @@
 
 from ttkthemes import ThemedTk
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+import ubikedata
 
 class Window(ThemedTk):
     # 自定義
     def __init__(self, theme:str='arc', **kwargs):
         # 呼叫父類別
         super().__init__(theme=theme, **kwargs)
+        try:
+            self.__data = ubikedata.load_data()
+        except Exception as e:
+            messagebox.showwarning(title='警告',message=str(e))
 
+        @property
+        def data(self) -> list[dict]:
+            return self.__data
 
 
 
