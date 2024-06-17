d9b80b213eecd5f8e34643df86ff97d6e95b5b54
Roger <roger02771@gmail.com>
2024-06-17 09:18:21 +0800

    ttk.Frame

--- 2024_06_17/lesson1.py
+++ 2024_06_17/lesson1.py
@@ -5,11 +5,21 @@ from tkinter import ttk, Misc
 class Example(ttk.Frame):
     def __init__(self, master:Misc, **kwargs):
         super().__init__(master=master, **kwargs)
+        master.title('Lines')
+        # 寫法 1:
+        # self.configure({'borderwidth':2, 'relief':'groove'})
+        # 寫法 2:
+        # self.config({'borderwidth':2, 'relief':'groove'})
+        # 寫法 3:
+        # self['borderwidth']=2
+        # self['relief']='groove'
+        self.pack(expand=True, fill='both')
 
 
 def main():
     window = tk.Tk()
     window.title("Frame的繼承")
+    Example(window)
     window.geometry("400x250")
     window.mainloop()
 
