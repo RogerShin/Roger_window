9493d364f6c6f274d1c72b0d1b9c9deff7ea86b5
Roger <roger02771@gmail.com>
2024-06-17 09:29:00 +0800

    ttk.Frame, tk.canvas

--- 2024_06_17/lesson1.py
+++ 2024_06_17/lesson1.py
@@ -7,12 +7,18 @@ class Example(ttk.Frame):
         super().__init__(master=master, **kwargs)
         master.title('Lines')
         # 寫法 1:
-        # self.configure({'borderwidth':2, 'relief':'groove'})
+        self.configure({'borderwidth':2, 'relief':'groove'})
         # 寫法 2:
         # self.config({'borderwidth':2, 'relief':'groove'})
         # 寫法 3:
         # self['borderwidth']=2
         # self['relief']='groove'
+        # 畫線
+        canvas = tk.Canvas(self)
+        canvas.create_line(15, 30, 200, 30)
+        canvas.create_line(300, 35, 300, 200, dash=(8, 2))
+        canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
+        canvas.pack(expand=True, fill='both')
         self.pack(expand=True, fill='both')
 
 
