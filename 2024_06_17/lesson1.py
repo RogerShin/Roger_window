57b0404366ef632d9a4a4573c4adf52467dd2c9a
Roger <roger02771@gmail.com>
2024-06-17 10:57:01 +0800

    ttk.Frame,  tk.canvas, from PIL import Image, ImageTk, import matplotlib.pyplot as plt, from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg

--- 2024_06_17/lesson1.py
+++ 2024_06_17/lesson1.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk, Misc
 from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 # 繼承ttk.Frame
 class Example(ttk.Frame):
@@ -33,7 +36,6 @@ class Example1(ttk.Frame):
         # 寫法 3:
         # self['borderwidth']=2
         # self['relief']='groove'
-        # 畫線
         canvas = tk.Canvas(self)
         # 畫方形, 填色
         canvas.create_rectangle(30, 10 ,120, 80, outline='#000', fill='#fb0')
@@ -59,17 +61,24 @@ class Example2(ttk.Frame):
         # 寫法 3:
         # self['borderwidth']=2
         # self['relief']='groove'
-        # 畫線
-        canvas = tk.Canvas(self)
-        
-        canvas.pack(expand=True, fill='both')
+        figure = plt.figure(figsize=(5, 4), dpi=30)
+        axes = figure.add_subplot()
+
+        axes.plot([1, 2, 3, 4, 5],[2, 3, 5, 7, 11])
+        axes.set_title("Sample Char")
+        axes.set_xlabel("X-axis")
+        axes.set_ylabel("Y-axis")
+
+        canvas = FigureCanvasTkAgg(figure, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(expand=True, fill='both', padx=30, pady=30)
         self.pack(expand=True, fill='both')
 
 def main():
     window = tk.Tk()
     window.title("Frame的繼承")
     Example2(window)
-    window.geometry("400x400")
+    window.geometry("600x500")
     window.mainloop()
 
 if __name__ == '__main__':
