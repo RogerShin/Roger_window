f0287e8f8063a2c7be1bceb7675693c3932372f3
Roger <roger02771@gmail.com>
2024-07-09 11:17:38 +0800

    create venv2, install flask, gunicorn

--- 2024_07_09/main.py
+++ 2024_07_09/main.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+# Use the route() decorator to bind a function to a URL.
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return "<h1>我的主題</h1>\n<h2>職能發展學院</h2>"
+
+# route("/")
+@app.route("/hello")
+def hello():
+    return '<h1>Hello, World</h1>'
