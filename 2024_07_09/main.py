3fa9ced8b46aefc8f32c79c66ad432f7796f3576
Roger <roger02771@gmail.com>
2024-07-09 16:35:20 +0800

    modify menu icon, jinja連接頁面 , 用jinja繼承base.html.jinja

--- 2024_07_09/main.py
+++ 2024_07_09/main.py
@@ -10,14 +10,14 @@ def index():
 
 @app.route("/new")
 def new():
-    return render_template("index.html.jinja")
+    return render_template("new.html.jinja")
 
 @app.route('/youbike')
 def youbike():
     # show the user profile for that user
-    return render_template("index.html.jinja")
+    return render_template("youbike.html.jinja")
 
 @app.route("/contact")
 def contact():
     # show the post with the given id, the id is an integer
-    return render_template("index.html.jinja")
+    return render_template("contact.html.jinja")
