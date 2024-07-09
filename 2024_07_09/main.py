b2292638351fcea20edcb1f27c325984ddcc8e29
Roger <roger02771@gmail.com>
2024-07-09 11:29:19 +0800

    create venv2, install flask, gunicorn, Use the route() decorator

--- 2024_07_09/main.py
+++ 2024_07_09/main.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 # Use the route() decorator to bind a function to a URL.
 @app.route("/")
 def index():
@@ -12,3 +11,13 @@ def index():
 @app.route("/hello")
 def hello():
     return '<h1>Hello, World</h1>'
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    # show the user profile for that user
+    return f"<h1>Hello! {(username)}<h1/>"
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f"<h1>Post {post_id}</h1>"
