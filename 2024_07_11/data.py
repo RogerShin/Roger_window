176ef4d4b23bcd99efc4b29bbb7d446115150a1c
Roger <roger02771@gmail.com>
2024-07-11 14:31:56 +0800

    dotenv, 讀取data資料

---
+++ 2024_07_11/data.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+
+
+# 不上傳Passwordkey
+load_dotenv()
+
+def get_areas():
+    # 建立連線
+    conn = psycopg2.connect(os.environ['POSTGRESQL_TOKEN'])
+    
+    with conn: #with conn會自動commit()
+       with conn.cursor() as cursor: # as cursor自動close()
+            sql='''
+                SELECT DISTINCT sarea FROM youbike;
+                
+            '''
+            cursor.execute(sql)
+            print(cursor.fetchall())
+
+
+    conn.close()
