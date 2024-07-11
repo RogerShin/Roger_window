6d1560e283fca7f287423cd8db3f5f7b4577d640
Roger <roger02771@gmail.com>
2024-07-11 16:35:59 +0800

    使用block 更改頁面內容, {{ super() }} style,  <form>, <select>, <option> {% for area in areas %}   <option> {% endfor %}迴圈, request.args.get, show_area=select_area

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -7,15 +7,13 @@ app = Flask(__name__)
 def index():
     # 方法1
     # print(list(map(lambda value:value[0], data.get_areas())))
-    
+
     # 方法2
     select_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
-    if select_area is None:
-        print("第一次進入")
-        return render_template("index.html.jinja", areas=areas)
-    else:
-        print(select_area)
-        return render_template("index.html.jinja", areas=areas)
+    
+    select_area= '士林區' if select_area is None else select_area
+        
+    return render_template("index.html.jinja", areas=areas, show_area=select_area)
 
    
