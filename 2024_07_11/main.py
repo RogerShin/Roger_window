27b04a6b263dd47149331864f67a9db21acff5ff
Roger <roger02771@gmail.com>
2024-07-15 10:30:22 +0800

    取得所有資料回傳到index.html.jinja

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -10,10 +10,15 @@ def index():
 
     # 方法2
     select_area = request.args.get('area')
+    # 從資料庫取得行政區資料
     areas = [tup[0] for tup in data.get_areas()]
-    
     select_area= '士林區' if select_area is None else select_area
-        
-    return render_template("index.html.jinja", areas=areas, show_area=select_area)
+    # 從資料庫取得該行政區所有站點資訊
+    detail_snaes = data.get_snaOfArea(area=select_area)
+
+    # areas -> 所有行政區
+    # show_area -> 要顯示的行政區
+    # detail_snaes -> 該行政區所有站點資訊
+    return render_template("index.html.jinja", areas=areas, show_area=select_area, detail_snaes=detail_snaes)
 
    
