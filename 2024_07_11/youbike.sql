7c9996b7c3cc588ba695cb4ef2775acd2a932ea7
Roger <roger02771@gmail.com>
2024-07-15 10:24:11 +0800

    -- 取站點最新資料

--- 2024_07_11/youbike.sql
+++ 2024_07_11/youbike.sql
@@ -1,4 +1,19 @@
-SELECT * FROM youbike
+SELECT * FROM youbike;
 
 
-SELECT DISTINCT sarea FROM youbike
+SELECT DISTINCT sarea FROM youbike;
+
+-- 取站點最新資料
+SELECT sna AS 站點, total AS 總車輛數, rent_bikes AS 可借, return_bikes AS 可還, mday AS 時間, act AS 狀態
+FROM youbike
+where (updatetime, sna) IN(
+	SELECT MAX (updatetime), sna
+		FROM youbike
+		where sarea = '大同區'
+		GROUP BY sna
+);
+
+SELECT MAX (updatetime), sna
+FROM youbike
+where sarea = '士林區'
+GROUP BY sna;
