a829b32f77cee42bcbf7143881140e999e01a888
Roger <roger02771@gmail.com>
2024-07-30 10:57:58 +0800

    modify

--- 2024_07_30_flask_dash/main.py
+++ 2024_07_30_flask_dash/main.py
@@ -36,7 +36,9 @@ def index1():
     # detail_snaes -> 該行政區所有站點資訊
     return render_template("index1.html.jinja", areas=areas, show_area=select_area, detail_snaes=detail_snaes)
 
-
+@app.errorhandler(404)
+def page_not_found(error):
+    return "<h2>沒有此頁面</h2>", 404
 
 if __name__ == "__main__":
     run_simple("localhost", 8080, application, use_debugger=True, use_reloader=True)   
